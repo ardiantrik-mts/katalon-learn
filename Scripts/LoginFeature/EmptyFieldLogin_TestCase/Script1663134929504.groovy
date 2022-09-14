337e318d0a23a431e0fe50368de9eec8c18e92d5
ardiantrik <ardiantrik@gmail.com>
2022-09-14 14:42:21 +0700

    add anothe login scenario

--- Scripts/LoginFeature/EmptyFieldLogin_TestCase/Script1663134929504.groovy
+++ Scripts/LoginFeature/EmptyFieldLogin_TestCase/Script1663134929504.groovy
@@ -21,11 +21,7 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-WebUI.setText(findTestObject('Page_OrangeHRM/input_Username_username'), 'Admin')
-
-WebUI.setEncryptedText(findTestObject('Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
-
-WebUI.click(findTestObject('Page_OrangeHRM/button_Login'))
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/button_Login'))
 
 WebUI.closeBrowser()
 
--- Test Suites/LoginFeature.ts
+++ Test Suites/LoginFeature.ts
@@ -12,7 +12,14 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>7815c93f-bab8-4572-b8a9-e827396c6a0f</testSuiteGuid>
    <testCaseLink>
-      <guid>dbd68480-5575-408d-8106-6f9e3ae5362d</guid>
+      <guid>1f42bbde-1e90-46da-9dba-f7900e5810b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginFeature/EmptyFieldLogin_TestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3a76194d-c995-4d39-bb97-b082eea59931</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/LoginFeature/SuccessLogin_TestCase</testCaseId>
