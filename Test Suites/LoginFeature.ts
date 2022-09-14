251ec519eaa724f69f1c8553a99b9646d5587bd9
ardiantrik <ardiantrik@gmail.com>
2022-09-14 12:09:05 +0700

    add katalon project

---
+++ Test Suites/LoginFeature.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginFeature</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7815c93f-bab8-4572-b8a9-e827396c6a0f</testSuiteGuid>
+   <testCaseLink>
+      <guid>dbd68480-5575-408d-8106-6f9e3ae5362d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginFeature/SuccessLogin_TestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
